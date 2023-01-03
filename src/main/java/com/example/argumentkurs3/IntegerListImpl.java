fa31f1bce0dbbc4a8c7aeec54861535ecf040a8b
Swetlana <sweta_zaika@mail.ru>
2023-01-03 16:26:20 +0300

    Аргументы часть вторая -часть тестов не работает дз-курс3

---
+++ src/main/java/com/example/argumentkurs3/IntegerListImpl.java
@@ -0,0 +1,238 @@
+package com.example.argumentkurs3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class IntegerListImpl implements IntegerList {
+    private Integer[] integerList;
+    private int size;
+
+    public IntegerListImpl(Integer[] integerList) {
+        this.integerList = integerList;
+    }
+
+    public IntegerListImpl() {
+        this.integerList = new Integer[10];
+    }
+
+    public IntegerListImpl(int initSize) {
+        this.integerList = new Integer[initSize];
+
+    }
+
+    @Override
+    public Integer add(Integer item) {
+        validateSize();
+        validateItem(item);
+        integerList[size++] = item;
+        return item;
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        validateSize();
+        validateItem(item);
+        validateIndex(index);
+        if (index == size) {
+            integerList[size++] = item;
+
+            return item;
+        }
+        System.arraycopy(integerList, index, integerList, index + 1, size - index);
+        integerList[index] = item;
+        size++;
+        return item;
+    }
+
+
+    @Override
+    public Integer set(int index, Integer item) {
+        validateIndex(index);
+        validateItem(item);
+        integerList[index] = item;
+        return item;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        validateItem(item);
+        int index = indexOf(item);
+
+        return remove(index);
+    }
+
+    @Override
+    public Integer remove(int index) {
+        validateIndex(index);
+        Integer item = integerList[index];
+        if (index != size) {
+            System.arraycopy(integerList, index + 1, integerList, index, size - index);
+        }
+        size--;
+        return item;
+    }
+
+
+    @Override
+    public boolean contains(Integer item) {
+        checkItem(item);
+        return IntegerListImpl.binarySearch(Arrays.copyOf(integerList, integerList.length), item) > -1;
+
+    }
+
+    private void checkItem(Integer item) {
+    }
+
+
+    @Override
+    public int indexOf(Integer item) {
+        for (int i = 0; i < size; i++) {
+            if (integerList[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (integerList[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Integer get(int index) {
+        validateIndex(index);
+        return integerList[index];
+    }
+
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return Arrays.copyOf(integerList, size);
+    }
+
+    private void validateItem(Integer item) {
+        if (item == null) {
+            throw new NullItemExeption();
+        }
+    }
+
+    private void validateSize() {
+        if (size == integerList.length) {
+            throw new StorageIsFullExeption();
+        }
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new InvalidIndexExeption();
+        }
+    }
+
+    private void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (i > 0 && arr[j - 1] >= temp) {
+                arr[i] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] integers1 = IntegerListImpl.toRondomArray();
+        Integer[] integers2 = IntegerListImpl.toRondomArray();
+        Integer[] integers3 = IntegerListImpl.toRondomArray();
+        long start1 = System.currentTimeMillis();
+        sortInsertion(integers1);
+        System.out.println("сортировка вситавки" + (System.currentTimeMillis() - start1));
+        long start2 = System.currentTimeMillis();
+        sortSelection(integers2);
+        System.out.println("сортировка выбором" + (System.currentTimeMillis() - start2));
+        long start3 = System.currentTimeMillis();
+        sortBubble(integers3);
+        System.out.println("сортировка пузырьком" + (System.currentTimeMillis() - start3));
+    }
+
+    // сортировка пузырьками
+    private static void sortBubble(Integer[] integers) {
+        for (int i = 0; i < integers.length - 1; i++) {
+            for (int j = 0; j < integers.length - 1 - i; j++) {
+                if (integers[j] > integers[j + 1]) {
+                    swapElements(integers, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static Integer[] toRondomArray() {
+        Integer[] integers = new Integer[100000];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = new Random().nextInt(1000);
+        }
+        return integers;
+    }
+
+    //сортировка вставкой
+    private static void sortInsertion(Integer[] integers) {
+        for (int i = 1; i < integers.length; i++) {
+            int temp = integers[i];
+            int j = i;
+            while (j > 0 && integers[j - 1] >= temp) {
+                integers[j] = integers[j - 1];
+                j--;
+            }
+            integers[j] = temp;
+        }
+    }
+
+    private static void swapElements(Integer[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+    // сортировка выбором
+    private static void sortSelection(Integer[] integers) {
+        for (int i = 0; i < integers.length - 1; i++) {
+            for (int j = 0; j < integers.length - 1 - i; j++) {
+                if (integers[j] > integers[j + 1]) {
+                    swapElements(integers, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    private static int binarySearch(Integer[] integers, Integer item) {
+        IntegerListImpl.sortInsertion(integers);
+        return Arrays.binarySearch(integers, item);
+    }
+}
--- src/test/java/com/example/argumentkurs3/StringListImplTest.java
+++ src/test/java/com/example/argumentkurs3/StringListImplTest.java
@@ -4,165 +4,191 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+class IntegerListImplTest {
+    private IntegerListImpl integers1;
+    private IntegerListImpl integers2;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class StringListImplTest {
-    private StringListImpl expected;
-    private StringListImpl actual;
-
+    //    @BeforeEach
+//    public void setUt() {
+//     //   var Integers = List.of("s1", "s2", "s3", "s4").toArray();
+//        integers1 = new IntegerListImpl(10);
+//        Integer Integer1 = Integer.valueOf("s1");
+//        Integer Integer2 = Integer.valueOf("s2");
+//        Integer Integer3 = Integer.valueOf("s3");
+//        Integer Integer4 = Integer.valueOf("s4");
+//        integers1.add(Integer1);
+//        integers1.add(Integer2);
+//        integers1.add(Integer3);
+//        integers1.add(Integer4);
+//        integers2 = new IntegerListImpl(10);
+//        integers2.add(Integer1);
+//        integers2.add(Integer2);
+//        integers2.add(Integer3);
+//        integers2.add(Integer4);
+//    }
     @BeforeEach
     public void setUt() {
-     //   var strings = List.of("s1", "s2", "s3", "s4").toArray();
-        expected = new StringListImpl(10);
-        String string1 = "s1";
-        String string2 = "s2";
-        String string3 = "s3";
-        String string4 = "s4";
-        expected.add(string1);
-        expected.add(string2);
-        expected.add(string3);
-        expected.add(string4);
-        actual = new StringListImpl(10);
-        actual.add(string1);
-        actual.add(string2);
-        actual.add(string3);
-        actual.add(string4);
+        //   var Integers = List.of("s1", "s2", "s3", "s4").toArray();
+        integers1 = new IntegerListImpl(4);
+        integers1.add(3);
+        integers1.add(1);
+        integers1.add(2);
+        integers1.add(4);
+        integers2 = new IntegerListImpl(4);
+        integers2.add(3);
+        integers2.add(1);
+        integers2.add(2);
+        integers2.add(4);
     }
 
     @Test
-    void add() {
-        StringListImpl stringList1 = new StringListImpl(10);
-        String string1 = "s1";
-        String string2 = "s2";
-        String string3 = "s3";
-        String string4 = "s4";
-        stringList1.add(string1);
-        stringList1.add(string2);
-        stringList1.add(string3);
-        stringList1.add(string4);
-//        String[] array=new String[10];
-//        array [0]="s1";
-//        array [1]="s2";
-//        array [2]="s3";
-//        array [3]="s4";
-        Assertions.assertEquals(expected,actual);
-
+    public void addInteger() {
+        Assertions.assertTrue(integers1.equals(integers2));
     }
 
     @Test
-    void testAdd() {
-        expected.add(1,"set");
-    //    expected = new StringListImpl(10);
-        String[] array = new String[10];
-        array[0] = "s1";
-        array[1] = "set";
-        array[2] = "s2";
-        array[3] = "s3";
-        array[4] = "s4";
-
-        actual.add(1, "set");
-        Assertions.assertArrayEquals(array,actual.toArray());
-
+    public void addByIndex() {
+        integers1.add(1,1);
+        integers2.add(1, 1);
+        Assertions.assertTrue(integers2.equals(integers1));
     }
+//        IntegerListImpl IntegerList1 = new IntegerListImpl(10);
+//        Integer Integer1 = Integer.valueOf("s1");
+//        Integer Integer2 = Integer.valueOf("s2");
+//        Integer Integer3 = Integer.valueOf("s3");
+//        Integer Integer4 = Integer.valueOf("s4");
+//        IntegerList1.add(Integer1);
+//        IntegerList1.add(Integer2);
+//        IntegerList1.add(Integer3);
+//        IntegerList1.add(Integer4);
+////        Integer[] array=new Integer[10];
+////        array [0]="s1";
+////        array [1]="s2";
+////        array [2]="s3";
+////        array [3]="s4";
+//        Assertions.assertEquals(integers1,integers2);
+//
+//    }
+
+//    @Test
+//    void testAdd() {
+//        integers1.add(1, Integer.valueOf("set"));
+//    //    expected = new IntegerListImpl(10);
+//        Integer[] array = new Integer[10];
+//        array[0] = Integer.valueOf("s1");
+//        array[1] = Integer.valueOf("set");
+//        array[2] = Integer.valueOf("s2");
+//        array[3] = Integer.valueOf("s3");
+//        array[4] = Integer.valueOf("s4");
+//
+//        integers2.add(1, Integer.valueOf("set"));
+//        Assertions.assertArrayEquals(array,integers2.toArray());
+//
+//    }
 
     @Test
-    void set() {
-        expected.set(1, "set");
-        actual.set(1, "set");
-        Assertions.assertEquals(expected, actual);
+    public void set() {
+        integers1.set(1, 1);
+        integers2.set(1, 1);
+        Assertions.assertTrue(integers1.equals(integers2));
     }
 
     @Test
     void remove() {
-        expected.remove(1);
-        actual.remove(1);
-        Assertions.assertEquals(expected, actual);
+        integers1.remove(1);
+        integers2.remove(1);
+        Assertions.assertTrue(integers1.equals(integers2));
     }
 
     @Test
-    void testRemove() {
-        expected.remove("s1");
-        actual.remove("s1");
-        Assertions.assertEquals(expected, actual);
+    void testRemoveByString() {
+        integers1.remove(1);
+        integers2.remove(1);
+        Assertions.assertTrue(integers1.equals(integers2));
     }
 
     @Test
-    void contains() {
-        Assertions.assertTrue(actual.contains("s1"));
+    public void contains() {
+        assert integers1.contains(1);
+        assert !integers1.contains(500);
     }
 
     @Test
     void indexOf() {
-        Assertions.assertEquals(expected.indexOf("s1"), 0);
+        Assertions.assertEquals(integers1.indexOf(1), 1);
     }
 
     @Test
     void lastIndexOf() {
-        Assertions.assertEquals(expected.indexOf("s1"), 0);
+        Assertions.assertEquals(integers1.indexOf(1), 1);
+
     }
 
     @Test
     void get() {
-        Assertions.assertEquals(expected.indexOf("s1"), 0);
+        Assertions.assertEquals(integers1.indexOf(1), 1);
     }
 
     @Test
     void testEquals() {
-        Assertions.assertTrue(expected.equals(actual));
+        Assertions.assertTrue(integers1.equals(integers2));
     }
 
     @Test
     void size() {
-        Assertions.assertEquals(expected.size(), actual.size());
+        Assertions.assertEquals(integers1.size(), integers2.size());
     }
 
     @Test
     void isEmpty() {
-        Assertions.assertFalse(expected.isEmpty());
+        Assertions.assertFalse(integers1.isEmpty());
     }
 
     @Test
     void clear() {
-        expected.clear();
-        Assertions.assertTrue(expected.isEmpty());
+        integers1.clear();
+        Assertions.assertTrue(integers1.isEmpty());
     }
 
     @Test
-    void toArray() {
-        String[] strings1 = expected.toArray();
-        String[] strings2 = new String[10];
-        strings2[0] = "s1";
-        strings2[1] = "s2";
-        strings2[2] = "s3";
-        strings2[4] = "s4";
-        Assertions.assertArrayEquals(strings1,strings2);
+    public void toArray() {
+        Integer[] integers1 = this.integers1.toArray();
+        Integer[] integers2 = new Integer[4];
+        integers2[0] = 3;
+        integers2[1] = 1;
+        integers2[2] = 2;
+        integers2[4] = 3;
+        Assertions.assertArrayEquals(integers1, integers2);
     }
 
     @Test
-    void shouldThrowIllegalArgumentExeptionWhenUseMethodAdd(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.add(0,null));
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.add(null));
+    void shouldThrowIllegalArgumentExeptionWhenUseMethodAdd() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.add(0, null));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.add(null));
     }
+
     @Test
-    void shouldThrowArrayIndexOutOfBoundsExceptioWhenUseMethodSet(){
-        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class,()->expected.set(11,"asd"));
+    void shouldThrowArrayIndexOutOfBoundsExceptioWhenUseMethodSet() {
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> integers1.set(11, Integer.valueOf("asd")));
     }
+
     @Test
-    void shouldThrowIllegalArgumentExeptionWhenUseMethodSet(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.set(0,null));
+    void shouldThrowIllegalArgumentExeptionWhenUseMethodSet() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.set(0, null));
     }
+
     @Test
-    void shouldThrowIllegalArgumentExeptionWhenUseMethodRemove(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.remove(null));
+    void shouldThrowIllegalArgumentExeptionWhenUseMethodRemove() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.remove(null));
     }
+
     @Test
-    void shouldThrowIllegalArgumentExeptionWhenUseMethodContains(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.contains(null));
+    void shouldThrowIllegalArgumentExeptionWhenUseMethodContains() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.contains(null));
     }
+
     @Test
-    void shouldThrowIllegalArgumentExeptionWhenUseMethodEquals(){
-        Assertions.assertThrows(IllegalArgumentException.class,()->expected.equals(null));
+    void shouldThrowIllegalArgumentExeptionWhenUseMethodEquals() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> integers1.equals(null));
     }
 }
